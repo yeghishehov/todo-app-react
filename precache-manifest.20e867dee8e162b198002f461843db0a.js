5344542da2a1a859f4216a2022fb4e2c07b26122
yeghishehov <yeghishehov.1@gmail.com>
2020-01-05 15:43:05 +0400

    Updates

---
+++ precache-manifest.20e867dee8e162b198002f461843db0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4b7d33b049c1c0815644b0dec7302e",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "a37b8766ffb99d0d6135",
+    "url": "/todo-app-react/static/css/main.a94001f6.chunk.css"
+  },
+  {
+    "revision": "79c61974c8f3008b67ef",
+    "url": "/todo-app-react/static/js/2.e4dac5bf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-app-react/static/js/2.e4dac5bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a37b8766ffb99d0d6135",
+    "url": "/todo-app-react/static/js/main.76fb431f.chunk.js"
+  },
+  {
+    "revision": "0c17d6c22e32b4f0299b",
+    "url": "/todo-app-react/static/js/runtime-main.256d4463.js"
+  }
+]);
